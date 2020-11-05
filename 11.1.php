f421afb35e2601f9c5c15300964c783b700121c9
aaron123-ioss <74001708+aaron123-ioss@users.noreply.github.com>
2020-11-05 23:24:51 +0800

    Add files via upload

---
+++ 11.1.php
@@ -0,0 +1,29 @@
+<?php
+/*ID: 602110193
+Name: yucaiwen
+Wechat: yucaiwen*/
+class multiplytable
+{  
+  public $limit;
+  
+  function __construct($par1)
+  {
+    $this->limit = $par1;
+  }
+  
+  function calculate($number)
+  {
+	for($i=1;$i<=$this->limit;$i++)
+	{
+		for($j=2;$j<=$number;$j++)
+		{
+			echo $i*$j." ";
+		}
+		echo"\n";
+	}
+  }
+}
+echo"Input the number :";
+$num=trim(fgets(STDIN));
+$a=new multiplytable(10);
+echo $a->calculate($num);
